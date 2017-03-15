0b1e76d9bb5bd2884ae9644899ce84d7a2f0cadf
Cong Ding <dinggnu@gmail.com>
2017-03-15 04:09:07 +0000

    fix

--- wechat-forwarding.py
+++ wechat-forwarding.py
@@ -16,45 +16,56 @@ sending_type = {'Picture': 'img', 'Video': 'vid'}
 data_path = 'data'
 from_group_names = {u'酒井 9#'}
 to_group_names = [u'酒井民间自救群', u'酒井 9# 二号']
-senders = {u'酒井9#'}
+nickname = ''
+bot = None
+
+if __name__ == '__main__':
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+    # if the QR code doesn't show correctly, you can try to change the value
+    # of enableCdmQR to 1 or -1 or -2. It nothing works, you can change it to
+    # enableCmdQR=True and a picture will show up.
+    bot = itchat.new_instance()
+    bot.auto_login(hotReload=True, enableCmdQR=2)
+    nickname = bot.loginInfo['User']['NickName']
 
 def get_sender_receiver(msg):
-    sender = None
-    receiver = None
+    sender = nickname
+    receiver = nickname
     if msg['FromUserName'][0:2] == '@@': # group chat
         sender = msg['ActualNickName']
-        m = itchat.search_chatrooms(userName=msg['FromUserName'])
+        m = bot.search_chatrooms(userName=msg['FromUserName'])
         if m is not None:
             receiver = m['NickName']
     elif msg['ToUserName'][0:2] == '@@': # group chat by myself
         if 'ActualNickName' in msg:
             sender = msg['ActualNickName']
         else:
-            m = itchat.search_friends(userName=msg['FromUserName'])
+            m = bot.search_friends(userName=msg['FromUserName'])
             if m is not None:
                 sender = m['NickName']
-        m = itchat.search_chatrooms(userName=msg['ToUserName'])
+        m = bot.search_chatrooms(userName=msg['ToUserName'])
         if m is not None:
             receiver = m['NickName']
     else: # personal chat
-        m = itchat.search_friends(userName=msg['FromUserName'])
+        m = bot.search_friends(userName=msg['FromUserName'])
         if m is not None:
             sender = m['NickName']
-        m = itchat.search_friends(userName=msg['ToUserName'])
+        m = bot.search_friends(userName=msg['ToUserName'])
         if m is not None:
             receiver = m['NickName']
-    return sender, receiver
+    return HTMLParser().unescape(sender), HTMLParser().unescape(receiver)
 
 def print_msg(msg):
     if len(msg) == 0:
         return
     print json.dumps(msg).decode('unicode-escape').encode('utf8')
 
-def get_whole_msg(msg, download=False, senders={}, receivers={}):
+def get_whole_msg(msg, download=False, receivers={}):
     if msg['FileName'][-4:] == 'gif': # can't handle gif pictures
         return []
     sender, receiver = get_sender_receiver(msg)
-    if (sender in senders) or (receiver not in receivers):
+    if (sender == nickname) or (receiver not in receivers):
         return []
     if len(msg['FileName']) > 0 and len(msg['Url']) == 0:
         if download: # download the file into data_path directory
@@ -79,25 +90,19 @@ def get_whole_msg(msg, download=False, senders={}, receivers={}):
         c += ' ' + url
     return ['[%s]: %s' % (sender, c)]
 
-@itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING,
+@bot.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING,
     ATTACHMENT, VIDEO, FRIENDS], isFriendChat=True, isGroupChat=True)
 def normal_msg(msg):
-    msg_send = get_whole_msg(msg, download=True, senders=senders, receivers=from_group_names)
+    msg_send = get_whole_msg(msg, download=True, receivers=from_group_names)
     if len(msg_send) == 0:
         return
     print_msg(msg_send)
     for m in msg_send:
         for name in to_group_names:
-            room = itchat.search_chatrooms(name=name)
+            room = bot.search_chatrooms(name=name)
             if room is not None and len(room) > 0:
                 username = room[0]['UserName']
-                itchat.send(m, toUserName=username)
+                bot.send(m, toUserName=username)
 
 if __name__ == '__main__':
-    if not os.path.exists(data_path):
-        os.mkdir(data_path)
-    # if the QR code doesn't show correctly, you can try to change the value
-    # of enableCdmQR to 1 or -1 or -2. It nothing works, you can change it to
-    # enableCmdQR=True and a picture will show up.
-    itchat.auto_login(hotReload=True, enableCmdQR=2)
-    itchat.run()
+    bot.run()
